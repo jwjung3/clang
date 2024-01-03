302db8acabac0ebac6bafa41a27a28ddb9dc7c6b
jwjung3 <155497814+jwjung3@users.noreply.github.com>
2024-01-03 10:48:35 +0900

    Add files via upload

---
+++ day01/input.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+
+	//º¯¼ö(variable)
+	int mega = 123;
+	printf("%d", mega);
+	return 0;
+}
---
+++ day01/output.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+	printf("정진우 21 컴퓨터공학과");
+	return 0;
+
+}
